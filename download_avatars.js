1c85f6951a891a36cb25c86df34883864011a425
trentingram <tingram@homeaway.com>
2019-01-15 23:57:57 +0000

    passed file into beautify.js

--- download_avatars.js
+++ download_avatars.js
@@ -7,62 +7,58 @@ const args = process.argv;
 // uses request library to pass url and headers via options, then handles errors, response, and body
 // passes parsed body data to callback for iteration
 function getRepoContributors(repoOwner, repoName, cb1) {
-    var options = {
-        url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
-        headers: {
-          'User-Agent': 'request'
-        },
-        'auth': {
-            'user': 'trentingram',
-            'pass': token.GITHUB_TOKEN
-          }
-      };
-    
-      request(options, function(err, response, body) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    },
+    'auth': {
+      'user': 'trentingram',
+      'pass': token.GITHUB_TOKEN
+    }
+  };
 
-          err ? console.log(err) :  null;
+  request(options, function(err, response, body) {
 
-          response ? console.log("Status code: " + response.statusCode) : null;
+    err ? console.log(err) : null;
 
-          if(body) {
-            const parsed = JSON.parse(body)
-            cb1(parsed)
-          }
-          });
-  }
+    response ? console.log("Status code: " + response.statusCode) : null;
 
-  // iterates through parsed body data and passed the url and path for download to file
+    if (body) {
+      const parsed = JSON.parse(body)
+      cb1(parsed)
+    }
+  });
+}
+
+// iterates through parsed body data and passed the url and path for download to file
 function loopThroughURLs(result, cb) {
-    
-    result.forEach(each => {
-        let url = each.avatar_url
-        let path = each.login
-        cb(url, path)
-    })
-  }
+
+  result.forEach(each => {
+    let url = each.avatar_url
+    let path = each.login
+    cb(url, path)
+  })
+}
 // accepts url and path and uses request library methods to write .jpg files to files system
-  function downloadImageByURL(url, filePath) {
-     
-    request.get(`${url}`)
+function downloadImageByURL(url, filePath) {
+
+  request.get(`${url}`)
     .on('error', function(err) {
-        console.log(err)
+      console.log(err)
     })
     .on('response', function(response) {
-        console.log("Downloading...")
-        // console.log(response.statusCode) // 200
-        // console.log(response.statusMessage)
-        // console.log(response.headers['content-type']) 
+      console.log("Downloading...")
+      // console.log(response.statusCode) // 200
+      // console.log(response.statusMessage)
+      // console.log(response.headers['content-type']) 
     })
     .pipe(fs.createWriteStream(`./avatars/${filePath}`))
-    .on('finish', function () {
-        console.log('Download finished.')
+    .on('finish', function() {
+      console.log('Download finished.')
     })
-  }
+}
 
-  getRepoContributors(owner, name, function (data) {
-    loopThroughURLs(data, downloadImageByURL)
-  })
-
-
-
-  
+getRepoContributors(owner, name, function(data) {
+  loopThroughURLs(data, downloadImageByURL)
+})
