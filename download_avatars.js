6449c7213922ab4b8d91be0e07a81f7cf378292c
trentingram <tingram@homeaway.com>
2019-01-15 02:46:36 +0000

    adds getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,10 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+    console.log(repoOwner, repoName)
+  }
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
