62496661e256fd528edadce555a1802446889267
trentingram <tingram@homeaway.com>
2019-01-15 03:21:17 +0000

    adds auth to request header

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,25 @@
 var request = require('request');
-
+var token = require('./secrets');
+console.log(token.GITHUB_TOKEN)
 function getRepoContributors(repoOwner, repoName, cb) {
-    console.log(repoOwner, repoName)
+    var options = {
+        url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+        headers: {
+          'User-Agent': 'request'
+        },
+        'auth': {
+            'user': 'trentingram',
+            'pass': token.GITHUB_TOKEN
+          }
+      };
+    
+      request(options, function(err, res, body) {
+        cb(err, body);
+      });
   }
 
   getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
   });
+  
